47c84c3044aea6e4e668e4f5b45873d2a11175ea
carder46 <100096994+carder46@users.noreply.github.com>
2022-02-22 12:13:50 +1300

    I did the first exercise

---
+++ 1_print_exercises.py
@@ -0,0 +1,34 @@
+#1_print_exercises.py
+
+#print out your name
+print("Carter Dermody")
+
+#print out your teachers name
+print("Luke Smith")
+
+#print out the name of your school
+print("Kings High School, Dunedin")
+
+#print out your name
+print("15")
+
+#print out the amount of hours you have on your favourite game
+print("394593")
+
+#print out what the year will be in 25 years
+print("2047")
+
+#print out who old you will be in 100 years
+print("115")
+
+#print out 658 / 7
+print(658/7)
+
+#print out how long ago was the first moon landing
+print(2022-1969)
+
+#print out your favourite game
+print("Red Dead Redemption 2")
+
+#print out your favourite class
+print("Digital Technology")
