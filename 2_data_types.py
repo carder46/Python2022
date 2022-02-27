5de49076d472989a991726bd9e07ddedf0c6fe31
carder46 <100096994+carder46@users.noreply.github.com>
2022-02-28 09:14:05 +1300

    Done exercise 2

---
+++ 2_data_types.py
@@ -0,0 +1,92 @@
+#1
+print("Ding ding pch n daa\nbam-ba-ba-re-bam\nbaram bom bom baba-bam-bam-bbommm\nwha-whats going on, on, on on?")
+
+#2
+print("\"Hasta la vista, baby.\" - Terminator")
+
+#3
+print("A string is anything put in between \"\" \(quotation marks\), this can be text, numbers, etc")
+
+#4
+print("An integer is a number")
+
+#5
+print("A float are numbers that have decimal points.")
+
+#6
+print(543/3)
+
+#7
+print(543//3)
+
+#8
+print(543%3)
+
+#9
+print(1000%9)
+
+#10
+print(434//10)
+
+#11
+print(3%2)
+
+#12
+print(5%5)
+
+#13
+print(555/12)
+
+#14
+print(123/2)
+
+#15
+print(123//2)
+
+#16
+print(123%2)
+
+#17
+print(764%2)
+
+#18
+print(165//2)
+
+#19
+print(10.0+10.0)
+
+#20
+print(2+4)
+
+#21
+print(3234.32497458345893745983579345794673460704673589634069347605897-4344)
+
+#22
+print(234-234324.223423423423423)
+
+#23
+print(23423423/222222222222)
+
+#24
+print(2*4.56)
+
+#25
+print(345*234)
+
+#26
+print("Carter"*10)
+
+#27
+print("Carter\n"*10)
+
+#28
+print("5+10=" + str(5+10))
+
+#29
+print("15/2=" + str(15/2))
+
+#30
+print("30/2*5=" + str(30.0/2.0*5.0))
+
+#31
+print("30/2*5=" + str(30/2*5))
