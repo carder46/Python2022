5b9b1e019ab44ce91611ef9e133ba67ac25c69bb
carder46 <100096994+carder46@users.noreply.github.com>
2022-02-28 09:52:15 +1300

    Completed exercise 4

---
+++ 3_variables.py
@@ -0,0 +1,42 @@
+#1
+playerlives = 1
+#2
+login = "Login : walterBONK"
+#3
+money = 23402643
+#4
+pnumber = "021324546"
+#5
+name = "Carter"
+#6
+age = 15
+#7
+year = 2022
+#8
+print(name)
+#9
+print(age)
+#10
+print(year)
+#11
+surname = "Dermody"
+#12
+school = "Kings High School"
+#13
+favfood = "Sushi"
+#14
+petname = "Eddie and Elsie"
+#15
+chocolate_blocks = 34534534
+#16
+player_score = 2
+#17
+highestscore = 2123
+#18
+lapscompleted = 12
+#19
+ammunition = 1200
+#20
+current_gear_value = 123945
+#21
+speed = "120km/h"
---
+++ 4_modifyingvariables.py
@@ -0,0 +1,44 @@
+#1
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+#2
+print("You have $"+str(money)+" in your bank account!")
+#3
+print("Completing transation for HAT")
+print("Transation Completed...")
+#4
+money = money-hat
+#5 + 6
+print("You now have $"+str(money)+" in your bank account!")
+#7 
+
+#TOP
+print("You have $"+str(money)+" in your bank account!")
+print("Completing transation for TOP")
+print("Transation Completed...")
+money = money-top
+print("You now have $"+str(money)+" in your bank account!")
+#PANTS
+print("You have $"+str(money)+" in your bank account!")
+print("Completing transation for PANTS")
+print("Transation Completed...")
+money = money-pants
+print("You now have $"+str(money)+" in your bank account!")
+#BELT
+print("You have $"+str(money)+" in your bank account!")
+print("Completing transation for BELT")
+print("Transation Completed...")
+money = money-belt
+print("You now have $"+str(money)+" in your bank account!")
+#SHOES
+print("You have $"+str(money)+" in your bank account!")
+print("Completing transation for SHOES")
+print("Transation Completed...")
+money = money-shoes
+print("You now have $"+str(money)+" in your bank account!")
+
+print("You have $"+str(money)+" left in your bank account, thank you for shopping with us!")
