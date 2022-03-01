daad673ca6d4a770cbfcda416b8f01081b036e9f
carder46 <100096994+carder46@users.noreply.github.com>
2022-03-02 12:21:02 +1300

    doing exercise 5

---
+++ 5_input.py
@@ -0,0 +1,10 @@
+#1
+keypress = input("Input any key then press enter.\n")
+print(f"Thank you, you may continue.")
+#2
+user_name = input("Hey there, what is your name? | REMEMBER : Press enter after entering your name!\n")
+print(f"Welcome to skyCloud Hosting, {user_name}. We hope you enjoy your time on our website.")
+#3
+user_age = int(input("Hey there, before you continue. What is your age? | REMEMBER : Press enter after entering your age.\n"))
+print(f"Thank you, you are {user_age}! You are young(ish).")
+#4
