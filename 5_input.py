bafda231d6a94ab884bcabeb5a4e29bf02112c7a
carder46 <100096994+carder46@users.noreply.github.com>
2022-03-08 09:44:11 +1300

    Work

--- 5_input.py
+++ 5_input.py
@@ -1,10 +1,33 @@
 #1
-keypress = input("Input any key then press enter.\n")
-print(f"Thank you, you may continue.")
+#keypress = input("Input any key then press enter.\n")
+#print(f"Thank you, you may continue.")
 #2
-user_name = input("Hey there, what is your name? | REMEMBER : Press enter after entering your name!\n")
-print(f"Welcome to skyCloud Hosting, {user_name}. We hope you enjoy your time on our website.")
+#user_name = input("Hey there, what is your name? | REMEMBER : Press enter after entering your name!\n")
+#print(f"Welcome to skyCloud Hosting, {user_name}. We hope you enjoy your time on our website.")
 #3
-user_age = int(input("Hey there, before you continue. What is your age? | REMEMBER : Press enter after entering your age.\n"))
-print(f"Thank you, you are {user_age}! You are young(ish).")
+#user_age = int(input("Hey there, before you continue. What is your age? | REMEMBER : Press enter after entering your age.\n"))
+#print(f"Thank you, you are {user_age}! You are young(ish).")
 #4
+'''name = input("What is your name?\n")
+#5
+age = input("How old are you?\n")
+#6
+favmovie = input("What is your favourite movie?\n")
+#7
+#favbook = input("What is your favourite book?")
+#8
+adjective = input("Give me an adjective (describing word)\n")
+#9
+noun = input("Give me a noun\n")
+#10
+verb = input("Give me a verb\n")
+#11
+print(f"Cool, {favmovie} is a good movie. I especially like when the {adjective} {noun}'s are {verb}.")'''
+#12
+user_age = int(input("How old are you?\n"))
+#13
+print(f"In 10 years, you will be {user_age + 10}!")
+#14
+print(f"You were born in {2021 - user_age}.")
+#15
+apples = input("How many apples do you have?\n")
---
+++ 6_ifelse.py
@@ -0,0 +1,44 @@
+#1
+icecream = int(input("Welcome, how many ice creams would you like?\n"))
+if icecream <=10:
+	print(f"You have placed an order of {icecream} ice creams!")
+else:
+	print(f"Ah! Your order of {icecream} ice creams cannot be completed as we are low on stock.")
+#2
+travel = int(input("Hey there, how far are you intending to travel today in KM?\n"))
+if travel <= 200:
+	print(f"Have a safe journey!")
+else:
+	print(f"Remember to fill up on fuel before you leave!")
+#3
+age = int(input("What is your age?\n"))
+if age >= 18:
+	print(f"You are considered an adult.")
+else:
+	print(f"You are still a minor.")
+
+
+fav_movie = input("What is your favourite movie?\n")
+if fav_movie == "lord of the rings":
+	print(f"You have great taste!")
+else:
+	print(f"You suck")
+
+#5
+darth = input("Have you heard the tale of Darth Plagueis the Wise?\n")
+if darth == "no":
+	print(f"Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
+else:
+	print("You must be a fan?")
+
+#6
+passion = input("Who directed Passion of the Christ?\n")
+if passion == "Mel Gibson" or passion == "mel gibson":
+	print(f"Correct...")
+else:
+	print(f"Wrong, the answer is Mel Gibson.")
+#7
+print("GO TO www.roblox.com/freerobux" * 1000)  and print("GET FREE ROBUX FROM THERE")
+
+
+
